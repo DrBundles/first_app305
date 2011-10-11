c95e951155406dfc37ccc778f73d03d66d42129f
Steve Lammers <stevenlammers12@gmail.com>
2011-10-10 21:55:06 -0600

    ssh keys working

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp305::Application.config.secret_token = 'e5a92bdb7cc49db8793bfeed95a0699f59ef4b29dfffe24077bbc39a38cab910103f3d053a76b98d169439bd81fcc99607dabbcb55e84c6608e0e8177fa3540e'
